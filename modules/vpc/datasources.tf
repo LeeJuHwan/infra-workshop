23fd2f444ed37ed60fc617d5ea559d589db3d655
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-01-20 23:09:48 +0900

    chore: init vpc module structure

---
+++ modules/vpc/datasources.tf
@@ -0,0 +1,14 @@
+data "aws_ami" "fck_nat" {
+  filter {
+    name   = "name"
+    values = ["fck-nat-al2023-*"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  owners      = ["568608671756"]
+  most_recent = true
+}
