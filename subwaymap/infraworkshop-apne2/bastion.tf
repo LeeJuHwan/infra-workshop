edb3273b4113078e3e71e556337060765a7482d5
이주환 <118493627+LeeJuHwan@users.noreply.github.com>
2025-01-26 10:39:48 +0900

    fix: change availability zone

--- subwaymap/infraworkshop-apne2/bastion.tf
+++ subwaymap/infraworkshop-apne2/bastion.tf
@@ -4,7 +4,7 @@ module "bastion" {
   bastion_instance_name = "${module.vpc.vpc_name}-bastion"
   instance_type         = "t3.micro"
   ami_id                = "ami-0a998385ed9f45655"
-  subnet_id             = module.vpc.subnets["infraworkshop-apne2-public-subnet-c"].id
+  subnet_id             = module.vpc.subnets["infraworkshop-apne2-management-subnet-c"].id
   security_group_id     = module.vpc.security_groups["infraworkshop-apne2-admin-permit-security-group"].id
   keypair_name          = "${module.vpc.vpc_name}-keypair"
   availability_zone     = "ap-northeast-2c"
